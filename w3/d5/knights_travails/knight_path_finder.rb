1a6cbeee59b678b0f41c9f4098035cc02151fbb9
kw-8 <kwu11235813@gmail.com>
2021-09-20 11:40:22 -0700

    phase 0 of knigts travails

--- w3/d5/knights_travails/knight_path_finder.rb
+++ w3/d5/knights_travails/knight_path_finder.rb
@@ -1,3 +1,17 @@
+require "poly_tree_node"
 class KnightPathFinder
-  
+  def self.valid_moves(pos)
+    
+  end
+
+  def initialize(starting_pos)
+    @root_node = PolyTreeNode.new(starting_pos)
+    @move_tree = build_move_tree
+  end
+
+  def build_move_tree
+  end
+
+  def find_path(end_pos)
+  end
 end
