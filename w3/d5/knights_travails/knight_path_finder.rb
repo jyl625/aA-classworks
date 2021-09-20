f52bac51566ebec043401cafdeafb81c8eac070c
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-09-20 14:30:08 -0700

    Rewrote build_move_tree method

--- w3/d5/knights_travails/knight_path_finder.rb
+++ w3/d5/knights_travails/knight_path_finder.rb
@@ -7,15 +7,15 @@ class KnightPathFinder
       [i-1, j-2], [i-1, j+2], [i+1, j-2],[i+1, j+2],
       [i-2, j-1], [i-2, j+1], [i+2, j-1],[i+2, j+1]
     ]
-    moves.select { |pair| pair.all? { |ele| ele.between(0, 7) }}
+    moves.select { |pair| pair.all? { |ele| ele.between?(0, 7) }}
   end
 
-  attr_reader :considered_positions
+  attr_reader :considered_positions, :root_node
   
   def initialize(starting_pos)
     @root_node = PolyTreeNode.new(starting_pos)
-    @move_tree = nil
     @considered_positions = [starting_pos]
+    self.build_move_tree
   end
 
   def new_move_positions(pos)
@@ -28,19 +28,35 @@ class KnightPathFinder
     new_positions
   end
 
-  def build_move_tree(target_pos)
+
+  def build_move_tree
     queue = [@root_node]
     until queue.empty?
       current_node = queue.shift
-      return current_node if current_node.value == target_pos
-      current_node.new_move_positions(current_node.value).each do |possible_move|
-        child_node = PolyTreeNode.new(possible_move)
-        child_node.parent = @current_node
+      new_moves = self.new_move_positions(current_node.value)
+      return if new_moves.empty?
+      new_moves.each do |pos|
+        child_node = PolyTreeNode.new(pos)
+        child_node.parent = current_node
         queue << child_node
       end
     end
     nil
   end
+
+  # def build_move_tree(target_pos)
+  #   queue = [@root_node]
+  #   until queue.empty?
+  #     current_node = queue.shift
+  #     return current_node if current_node.value == target_pos
+  #     current_node.new_move_positions(current_node.value).each do |possible_move|
+  #       child_node = PolyTreeNode.new(possible_move)
+  #       child_node.parent = @current_node
+  #       queue << child_node
+  #     end
+  #   end
+  #   nil
+  # end
 
   def find_path(end_pos)
   end
