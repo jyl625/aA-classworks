c252ad9db31ca903b016c638f7417dc74f29f87b
kw-8 <kwu11235813@gmail.com>
2021-09-20 15:32:55 -0700

    first commit: children take 1, initialize

--- w4/d1/tic-tac-toe_AI/lib/tic_tac_toe_node.rb
+++ w4/d1/tic-tac-toe_AI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,11 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,14 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_arr = []
+    (0...3).each do |row|
+      (0...3).each do |col|
+        new_board = self.board.dup
+        pos = [row,col]
+        new_board[pos] = next_mover_mark if new_board.empty?(pos)
+      end
+    end
+    children_arr
   end
 end
