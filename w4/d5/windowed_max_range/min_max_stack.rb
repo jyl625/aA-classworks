8624aa2d313e57496ee112170a1c394beb77b85e
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-09-25 23:08:14 -0700

    Add #push, #curr_min, #curr_max, and #peek methods

--- w4/d5/windowed_max_range/min_max_stack.rb
+++ w4/d5/windowed_max_range/min_max_stack.rb
@@ -9,11 +9,34 @@ class MinMaxStack
     @store.empty?
   end
 
-  def push(item)
-    
+  def push(val)
+    @store.push( {
+      value: val
+      min: curr_min(val)
+      max: curr_max(val)
+    })
   end
 
   def peek
+    @store.peek[:value] unless empty?
+  end
 
+
+  private
+
+  def curr_min(val)
+    if empty?
+      val
+    else
+      val < min ? val : min
+    end
+  end
+
+  def curr_max(val)
+    if empty?
+      val
+    else
+      val > max ? : val : max
+    end
   end
 end
