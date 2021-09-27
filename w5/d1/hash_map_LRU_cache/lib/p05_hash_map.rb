10e1b16a047e8ddaeb3e2e4c3e7ddb18bf0d955d
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-09-27 16:37:33 -0700

    Complete part 5 hash map

--- w5/d1/hash_map_LRU_cache/lib/p05_hash_map.rb
+++ w5/d1/hash_map_LRU_cache/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable #WHY!?
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,10 +11,17 @@ class HashMap
   end
 
   def include?(key)
+    @store[bucket(key)].include?(key)
   end
-
+  
   def set(key, val)
-    @store[bucket(key)].append(key, val)
+    if include?(key)
+      @store[bucket(key)].update(key,val)
+    else
+      resize! if @count == num_buckets
+      @store[bucket(key)].append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
@@ -20,18 +29,26 @@ class HashMap
   end
 
   def delete(key)
+    @store[bucket(key)].remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        prc.call(node.key, node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -43,6 +60,15 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) {LinkedList.new}
+
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        new_bucket_key = node.key.hash % new_store.length
+        new_store[new_bucket_key].append(node.key, node.val)
+      end
+    end
+    @store = new_store
   end
 
   def bucket(key)
