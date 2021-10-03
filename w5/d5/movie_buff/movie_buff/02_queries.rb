a6831de9b6bd69c5f8f823443df4cd882fb47dc2
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-02 17:24:31 -0700

    define eighties_b_movies and bad_years methods

--- w5/d5/movie_buff/movie_buff/02_queries.rb
+++ w5/d5/movie_buff/movie_buff/02_queries.rb
@@ -2,35 +2,25 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(score: (3..5), yr: (1980..1989))
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
 
-  m = Movie.find_by_sql(<<-SQL)
-    SELECT DISTINCT
-      movies.yr
-    FROM  
-      movies
-    WHERE
-      movies.yr NOT IN (
-        SELECT DISTINCT
-          movies.yr
-        FROM
-          movies
-        WHERE
-          movies.score > 8
-      )
-  SQL
-  m.pluck(:yr)
-  
+  Movie
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+
 
 end
 
