6f49b7bbdf74a2bff495b1abc95f047e4d2db4bd
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-12 13:28:23 -0700

    Start implementationg of Session controller and views

--- w7/d2/music_app/app/controllers/sessions_controller.rb
+++ w7/d2/music_app/app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def new
+    render :new
   end
 
   def create
+    
   end
 
   def destroy
--- w7/d2/music_app/config/routes.rb
+++ w7/d2/music_app/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
   resource :session, only: [:new, :create, :destroy]
+
+  root to: 'sessions#new'
 end
