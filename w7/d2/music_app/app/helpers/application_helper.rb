48a3448f652b93bc076cbfba5d9f8cd71129d5dd
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-14 18:44:30 -0700

    Create AA Times project

--- w7/d2/music_app/app/helpers/application_helper.rb
+++ w7/d2/music_app/app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   def auth_token
     # '<input type="hidden" name="authenticity_token" value="<%=form_authenticity_token%>">'.html_safe #sometimes doesn't work
     # "<input type=\"hidden\" name=\"authenticity_token\" value=\"<%=form_authenticity_token%>\">".html_safe #sometimes doesn't works
-    "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe #Always works
+    # "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe #Always works
+    return '<input type="hidden" name="authenticity_token" value="<%= form_authenticity_token %>"/>'.html_safe 
   end
 end
