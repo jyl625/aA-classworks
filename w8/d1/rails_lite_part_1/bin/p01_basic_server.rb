947c47d51c24cd2ab07e12750811c4afaf1c2d3c
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-18 17:03:36 -0700

    Complete phase 1 Rails Lite

--- w8/d1/rails_lite_part_1/bin/p01_basic_server.rb
+++ w8/d1/rails_lite_part_1/bin/p01_basic_server.rb
@@ -1,1 +1,22 @@
 require 'rack'
+
+
+app = Proc.new do |env|
+  puts env
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+
+  res['Content-Type'] = 'text/html'
+  if req.path == "/i/love/app/academy"
+    res.write("/i/love/app/academy")
+  else
+    res.write("Hello world!")
+  end
+  res.finish
+end
+
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
