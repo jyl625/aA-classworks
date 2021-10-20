88a14ed522de1a9741a3b95b1ae8b8a2ad09e5ea
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-19 17:49:40 -0700

    Complete upto phase_4 fibonacci method

--- w8/d2/intro_js/phase_3_iteration.js
+++ w8/d2/intro_js/phase_3_iteration.js
@@ -1,1 +1,55 @@
-Array.prototype.
+Array.prototype.bubbleSort = function() {
+  let sorted = false;
+
+  while (!sorted){
+    sorted = true;
+
+    for(let i = 0; i <this.length - 1; i++){
+      if (this[i] > this[i+1]){
+        let temp = this[i];
+        this[i] = this[i + 1];
+        this[i + 1] = temp;
+        sorted = false;
+      }
+    }
+  }
+  return this;
+}
+
+// let arr = [5,2,-4,2,8,1];
+// console.log(arr.bubbleSort());
+
+// String.prototype.substrings = function() {
+//   let result = [];
+//   for(let i = 0; i < this.length; i++) {
+//     for(let j = 1; j <= this.length; j++) {
+//       if (j>i){
+//         result.push(this.slice(i,j));
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+String.prototype.substrings = function () {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i+1; j <= this.length; j++) {
+      result.push(this.slice(i, j));
+    }
+  }
+  return result;
+}
+
+// String.prototype.substrings = function () {
+//   let result = [];
+//   for (let i = 0; i < this.length; i++) {
+//     for (let j = i; j < this.length; j++) {
+//       result.push(this.slice(i, j+1));
+//     }
+//   }
+//   return result;
+// }
+
+let str = "cat";
+console.log(str.substrings());
---
+++ w8/d2/intro_js/phase_4_recursion.js
@@ -0,0 +1,64 @@
+function range(start, end) {
+  if (start === end) { return [end]; }
+  return [start].concat(range(start+1, end));
+}
+
+// console.log(range(0,5));
+
+function sumRec(arr) {
+  if (arr.length === 1) { return arr[0]; }
+  return arr[0] + sumRec(arr.slice(1))
+}
+
+// console.log(sumRec([1,2,3,4,5]));
+
+//version 1
+// function exponent(base, exp) {
+//   if (exp === 0) {return 1;}
+//   return base * exponent(base, exp - 1);
+// }
+
+//version 2
+function exponent(base, exp) {
+  if (exp === 0) { return 1; }
+  if (exp % 2 === 0) {
+    return exponent(base, exp/2) ** 2;
+  } else {
+    return base * (exponent(base, (exp - 1)/2) ** 2);
+  }
+}
+
+// console.log(exponent(2, 4));
+
+
+
+
+// function fibonacci(n) {
+//   if (n === 2) {return [0,1];}
+//   if (n === 1) {return [0];}
+//   if (n === 0) {return []; }
+
+//   prev_fib = fibonacci(n - 1);
+//   next_num = prev_fib[prev_fib.length - 1] + prev_fib[prev_fib.length - 2];
+
+//   prev_fib.push(next_num);
+
+//   return prev_fib;
+// }
+
+function fibonacci(n) {
+  if (n <= 2) { return [0, 1].slice(0,n); }
+
+  prev_fib = fibonacci(n - 1);
+  next_num = prev_fib[prev_fib.length - 1] + prev_fib[prev_fib.length - 2];
+
+  prev_fib.push(next_num);
+
+  return prev_fib;
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
