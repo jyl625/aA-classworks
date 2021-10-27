b134fdf6e93a9f4c6bd5dfc69a51752d6391b927
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-26 17:30:37 -0700

    Finish game implementation

--- w9/d2/browser_tictactoe/src/index.js
+++ w9/d2/browser_tictactoe/src/index.js
@@ -1,6 +1,13 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');
+const Game = require('../ttt_node/game.js');
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  let game = new Game();
+
+  const el = document.querySelector(".ttt");
+
+  let view = new View(game, el);
+
+  // ul.addEventListener("click", view.handleClick);
+  //view.bindEvents()
 });
