5d418e5f041c1efdf0fd09c85ea621574c8f8050
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-11-06 19:52:03 -0700

    Define Clock#render method to  correctly render clock title in the index.html

--- w10/d5/widgets/frontend/clock.jsx
+++ w10/d5/widgets/frontend/clock.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 class Clock extends React.Component {
-  
+
+  render() {
+    return (
+      <h1>Clock Title</h1>
+    )
+  }
 }
 
 export default Clock;
--- w10/d5/widgets/root.jsx
+++ w10/d5/widgets/root.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Clock from './frontend/clock';
 
-const Root = () => <h1>Congratulations, you did it!</h1>;
+const Root = () => {
+  return (
+    <div>
+      <h1>Welcome to Awesome Widgets</h1>
+      < Clock />
+    </div>
+  )
+}  
 
 export default Root;
