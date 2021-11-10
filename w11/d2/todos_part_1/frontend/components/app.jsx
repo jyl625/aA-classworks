2b0fcc5ebcd4d952c1a5d86de920fffc0cabe3b2
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-11-10 13:33:21 -0800

    Create app.jsx, and  root.jsx

---
+++ w11/d2/todos_part_1/frontend/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <h1>Here's the app!</h1>
+);
+
+export default App;
--- w11/d2/todos_part_1/todo_redux.jsx
+++ w11/d2/todos_part_1/todo_redux.jsx
@@ -1,20 +1,24 @@
 import configureStore from './frontend/store/store'
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Root from './frontend/components/root'
+
 import { receiveTodos, receiveTodo, removeTodo } from './frontend/actions/todo_actions.js'
 import { receiveSteps, receiveStep, removeStep } from './frontend/actions/step_actions.js'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Todos App</h1> , root)
-
 
   const store = configureStore();
+  ReactDOM.render(<Root store={store} /> , root)
+
+
   window.store = store
   window.receiveTodos = receiveTodos;
   window.receiveTodo = receiveTodo;
   window.removeTodo = removeTodo;
-  
+
   window.receiveSteps = receiveSteps;
   window.receiveStep = receiveStep;
   window.removeStep = removeStep;
