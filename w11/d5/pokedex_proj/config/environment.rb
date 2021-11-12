446f7388ba79567e93601cea1008fa57e529765b
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-11-12 14:43:19 -0800

    Set up API Util, action creators, pokemonReducer, entityReducer, rootReducer and store

--- w11/d5/pokedex_proj/config/environment.rb
+++ w11/d5/pokedex_proj/config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Convert snake_case keys to camelCase.
+Jbuilder.key_format camelize: :lower
--- w11/d5/pokedex_proj/frontend/pokedex.jsx
+++ w11/d5/pokedex_proj/frontend/pokedex.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+// ========= TESTING ONLY ========= //
+import {receiveAllPokemon} from './actions/pokemon_actions'
+import {fetchAllPokemon} from './util/api_util'
+// ================================ //
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log("Wecelcome to PokeDex");
+
   const rootEle = document.getElementById('root');
+  const store = configureStore();
+
   ReactDOM.render(<h1>Pokedex</h1>, rootEle);
+
+  // ========= TESTING ONLY ========= //
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // ================================ //
 })
