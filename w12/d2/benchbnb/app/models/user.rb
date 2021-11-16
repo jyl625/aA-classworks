9ef5e7a73abcc81f4ee87ba67efb4f81ab08c1a2
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-11-16 15:01:37 -0800

    Finished user model

---
+++ w12/d2/benchbnb/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_valid_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_valid_password?(password)
+    pw_obj = BCrypt::Password.new(password_digest)
+    pw_obj.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+end
